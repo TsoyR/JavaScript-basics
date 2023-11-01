d2493a872d56544a9f810784dab1e1884f7919e7
roman <tsoyroman29@gmail.com>
2023-11-02 08:19:46 +0900

    change

--- 4/1.js
+++ 4/1.js
@@ -13,7 +13,7 @@
 for (let i = 0; i < 11; i++) {
   if (i === 0) {
     console.log(`${i} - это ноль`);
-  } else if (i % 2 === 0 && i > 0) {
+  } else if (i % 2 === 0) {
     console.log(`${i} - четное число`);
   } else {
     console.log(`${i} - нечетное число`);
